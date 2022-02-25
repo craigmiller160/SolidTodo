edef26b5d7f067b43a9d940f509306c5afb813a3
Craig Miller <craigmiller160@gmail.com>
2022-02-25 15:23:57 -0500

    Got the routes working

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,14 @@
-import { useRoutes } from 'solid-app-router';
+import { useRoutes, Router } from 'solid-app-router';
 import { routes } from '../routes';
 
 export const App = () => {
 	const Routes = useRoutes(routes);
 	return (
-		<div>
-			<h1>Hello World</h1>
-			<Routes />
-		</div>
+		<Router>
+			<div>
+				<h1>Hello World</h1>
+				<Routes />
+			</div>
+		</Router>
 	);
 };
