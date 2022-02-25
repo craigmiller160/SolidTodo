fd967997812e784762caa5d409ab91bf0e40388a
Craig Miller <craigmiller160@gmail.com>
2022-02-25 15:56:32 -0500

    got dynamic list generation

--- src/components/TodoList/TodoCard/index.tsx
+++ src/components/TodoList/TodoCard/index.tsx
@@ -1,12 +1,17 @@
 import './TodoCard.scss';
+import { Todo } from '../../../types/Todo';
 
-export const TodoCard = () => (
+interface Props {
+	readonly todo: Todo;
+}
+
+export const TodoCard = (props: Props) => (
 	<div class="TodoCard">
 		<div class="TodoCheckbox">
 			<input type="checkbox" name="complete" />
 		</div>
 		<div class="TodoTitle">
-			<h2>Title</h2>
+			<h2>{props.todo.title}</h2>
 		</div>
 	</div>
 );
--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,9 +1,30 @@
 import { TodoCard } from './TodoCard';
+import { Todo } from '../../types/Todo';
+import { For } from 'solid-js';
+
+const tempList: ReadonlyArray<Todo> = [
+	{
+		timestamp: '2022-01-01T00:00:00',
+		title: 'First TODO',
+		description: '',
+		isComplete: false
+	},
+	{
+		timestamp: '2022-01-02T00:00:00',
+		title: 'Second TODO',
+		description: '',
+		isComplete: true
+	},
+	{
+		timestamp: '2022-01-03T00:00:00',
+		title: 'Third TODO',
+		description: '',
+		isComplete: false
+	}
+];
 
 export const TodoList = () => (
 	<div>
-		<TodoCard />
-		<TodoCard />
-		<TodoCard />
+		<For each={tempList}>{(todo) => <TodoCard todo={todo} />}</For>
 	</div>
 );
