df6f45f6b09c2317e4eb4fea0cd840a7b809580a
Craig Miller <craigmiller160@gmail.com>
2022-02-25 17:46:38 -0500

    Got the store setup and integrated

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,28 +1,7 @@
 import { TodoCard } from './TodoCard';
-import { Todo } from '../../types/Todo';
 import { For } from 'solid-js';
 import { useNavigate } from 'solid-app-router';
-
-const tempList: ReadonlyArray<Todo> = [
-	{
-		timestamp: '2022-01-01T00:00:00',
-		title: 'First TODO',
-		description: '',
-		isComplete: false
-	},
-	{
-		timestamp: '2022-01-02T00:00:00',
-		title: 'Second TODO',
-		description: '',
-		isComplete: true
-	},
-	{
-		timestamp: '2022-01-03T00:00:00',
-		title: 'Third TODO',
-		description: '',
-		isComplete: false
-	}
-];
+import { todoStore } from '../../store/todoStore';
 
 export const TodoList = () => {
 	const navigate = useNavigate();
@@ -30,7 +9,9 @@ export const TodoList = () => {
 	return (
 		<div>
 			<div>
-				<For each={tempList}>{(todo) => <TodoCard todo={todo} />}</For>
+				<For each={todoStore.todos}>
+					{(todo) => <TodoCard todo={todo} />}
+				</For>
 			</div>
 			<button type="button" onClick={addTodo}>
 				Add TODO
---
+++ src/store/todoStore.ts
@@ -0,0 +1,29 @@
+import { createStore } from 'solid-js/store';
+import { Todo } from '../types/Todo';
+
+export interface TodoStore {
+	readonly todos: ReadonlyArray<Todo>;
+}
+
+export const [todoStore, setTodoStore] = createStore<TodoStore>({
+	todos: [
+		{
+			timestamp: '2022-01-01T00:00:00',
+			title: 'First TODO',
+			description: '',
+			isComplete: false
+		},
+		{
+			timestamp: '2022-01-02T00:00:00',
+			title: 'Second TODO',
+			description: '',
+			isComplete: true
+		},
+		{
+			timestamp: '2022-01-03T00:00:00',
+			title: 'Third TODO',
+			description: '',
+			isComplete: false
+		}
+	]
+});
