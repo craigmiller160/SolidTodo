286a4d262fd2bba8a1743ac474063d81866d4c9a
Craig Miller <craigmiller160@gmail.com>
2022-02-25 15:01:18 -0500

    Setting up eslint

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+    plugins: ["solid"],
+    parser: '@typescript-eslint/parser',
+    extends: ["eslint:recommended", "plugin:solid/recommended", 'plugin:@typescript-eslint/recommended']
+};
