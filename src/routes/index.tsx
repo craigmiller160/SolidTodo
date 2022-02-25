d92133945da4fdd6c1f330531547799937a095eb
Craig Miller <craigmiller160@gmail.com>
2022-02-25 15:23:05 -0500

    Getting router configured

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,9 @@
-import * as a from 'solid-app-router';
 import { RouteDefinition } from 'solid-app-router';
+import { Home } from '../components/Home';
 
 export const routes: RouteDefinition[] = [
-
+	{
+		path: '/',
+		component: Home
+	}
 ];
